898181ebafdf35b520cf457b7197da0e1951ca7b
renanp96 <renanpa1996@gmail.com>
2022-01-26 14:29:23 -0300

    Endpoint para recuperacao de senha implementado

--- src/main/java/com/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/cursomc/services/AbstractEmailService.java
@@ -14,6 +14,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -72,4 +73,19 @@ public abstract class AbstractEmailService implements EmailService{
 		return mimeMessage;
 	}
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Sollicitação de nova senha.");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha -> " + newPass);
+		return sm;
+	}
 }
